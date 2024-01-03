d3a023e97dadf5585484dd9703dcf49988e86bbe
Matias <selinmatias5@gmail.com>
2024-01-03 19:16:24 +0200

    send correct moves to websocket

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config';
+import open from 'open';
 import WebSocket from 'ws';
-import { GameInstance, GameState, Position } from './types';
+import { Position } from './types';
 import { aStar, createGraph, pathToDirections } from './astar';
 
 const playerToken = process.env.PLAYER_TOKEN || 'null';
@@ -51,8 +52,17 @@ const runApp = async () => {
   console.log('Maze:');
   prettyPrintMaze(gameState.maze);
 
+  console.log('Start: ', gameState.start);
+
+  const fixedTarget = {
+    x: gameState.target.y,
+    y: gameState.target.x,
+  }; // For some reason, the target is flipped somewhere
+
+  console.log('Target: ', fixedTarget);
+
   const graph = createGraph(gameState.maze);
-  const solution = aStar(graph, gameState.start, gameState.target);
+  const solution = aStar(graph, gameState.start, fixedTarget);
 
   if (!solution) {
     console.log('No solution found!');
@@ -62,12 +72,87 @@ const runApp = async () => {
   console.log('Solution:');
   prettyPrintSolution(gameState.maze, solution);
 
+  const moves = pathToDirections(solution);
+  console.log(moves);
+
   const ws = new WebSocket(`wss://goldrush.monad.fi/backend/${playerToken}/`);
 
-  ws.on('open', () => {
+  ws.on('open', async () => {
     console.log('Websocket opened!');
     ws.send(JSON.stringify(['sub-game', { id: game.entityId }]));
+
+    open(`https://goldrush.monad.fi/?id=${game.entityId}`);
+
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+
+    const firstMove = moves[0];
+    ws.send(
+      JSON.stringify([
+        'run-command',
+        {
+          gameId: game.entityId,
+          payload: {
+            action: 'rotate',
+            rotation: directionToRotation(firstMove),
+          },
+        },
+      ])
+    ); // First, turn to the correct direction
+
+    console.log('Sent first rotation: ', firstMove);
+
+    let previousMove = firstMove;
+
+    while (true) {
+      await new Promise((resolve) => setTimeout(resolve, 100));
+
+      const move = moves.shift();
+
+      if (!move) {
+        console.log('No more moves!');
+        break;
+      }
+
+      if (move === previousMove) {
+        console.log('Moving forward: ', move);
+        ws.send(
+          JSON.stringify([
+            'run-command',
+            { gameId: game.entityId, payload: { action: 'move' } },
+          ])
+        ); // If we are moving in the same direction as before, just move
+      } else {
+        console.log('Turning in direction: ', move);
+        ws.send(
+          JSON.stringify([
+            'run-command',
+            {
+              gameId: game.entityId,
+              payload: {
+                action: 'rotate',
+                rotation: directionToRotation(move),
+              },
+            },
+          ])
+        );
+        moves.unshift(move); // Otherwise, turn to the correct direction and add the move back to the queue
+      }
+      previousMove = move;
+    }
   });
+};
+
+const directionToRotation = (direction: string) => {
+  switch (direction) {
+    case 'N':
+      return 0;
+    case 'E':
+      return 90;
+    case 'S':
+      return 180;
+    case 'W':
+      return 270;
+  }
 };
 
 export const intToBinaryString = (n: number) => {
