31f00fa7f66538795b2f0905b852ac850d8624a1
Matias <selinmatias5@gmail.com>
2024-01-03 17:17:47 +0200

    pretty print maze

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
 import 'dotenv/config';
 import WebSocket from 'ws';
-import { GameInstance } from './types';
+import { GameInstance, GameState } from './types';
 
 const playerToken = process.env.PLAYER_TOKEN || 'null';
 const levelId = process.env.LEVEL_ID || 'null';
+
+const prettyPrintMaze = (maze: number[][]) => {
+  for (let row of maze) {
+    console.log(
+      row
+        .map(intToBinaryString)
+        .map(openDirections)
+        .map(directionsToBoxDrawings)
+        .join(' ')
+    );
+  }
+};
 
 const runApp = async () => {
   console.log('Player token: ', playerToken);
@@ -17,7 +29,12 @@ const runApp = async () => {
     { method: 'POST', headers }
   );
 
-  const game = (await response.json()) as GameInstance;
+  const game = await response.json();
+
+  const gameState = JSON.parse(game.gameState);
+
+  console.log('Maze:');
+  prettyPrintMaze(gameState.maze);
 
   const ws = new WebSocket(`wss://goldrush.monad.fi/backend/${playerToken}/`);
 
@@ -25,6 +42,63 @@ const runApp = async () => {
     console.log('Websocket opened!');
     ws.send(JSON.stringify(['sub-game', { id: game.entityId }]));
   });
+};
+
+const intToBinaryString = (n: number) => {
+  return n.toString(2).padStart(4, '0');
+};
+
+const openDirections = (bitString: string) => {
+  const directions: string[] = [];
+
+  if (bitString.charAt(0) === '0') {
+    directions.push('N');
+  }
+  if (bitString.charAt(1) === '0') {
+    directions.push('E');
+  }
+  if (bitString.charAt(2) === '0') {
+    directions.push('S');
+  }
+  if (bitString.charAt(3) === '0') {
+    directions.push('W');
+  }
+
+  return directions;
+};
+
+const directionsToBoxDrawings = (directions: string[]) => {
+  if (['N', 'E', 'S', 'W'].every((d) => directions.includes(d))) {
+    return '┼';
+  } else if (['N', 'E', 'S'].every((d) => directions.includes(d))) {
+    return '├';
+  } else if (['N', 'E', 'W'].every((d) => directions.includes(d))) {
+    return '┴';
+  } else if (['N', 'S', 'W'].every((d) => directions.includes(d))) {
+    return '┤';
+  } else if (['E', 'S', 'W'].every((d) => directions.includes(d))) {
+    return '┬';
+  } else if (['N', 'E'].every((d) => directions.includes(d))) {
+    return '└';
+  } else if (['N', 'S'].every((d) => directions.includes(d))) {
+    return '│';
+  } else if (['N', 'W'].every((d) => directions.includes(d))) {
+    return '┘';
+  } else if (['E', 'S'].every((d) => directions.includes(d))) {
+    return '┌';
+  } else if (['E', 'W'].every((d) => directions.includes(d))) {
+    return '─';
+  } else if (['S', 'W'].every((d) => directions.includes(d))) {
+    return '┐';
+  } else if (directions.includes('N')) {
+    return '╵';
+  } else if (directions.includes('E')) {
+    return '╶';
+  } else if (directions.includes('S')) {
+    return '╷';
+  } else if (directions.includes('W')) {
+    return '╴';
+  }
 };
 
 runApp();
--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,31 @@
 export type GameInstance = {
-  gameState: string;
+  gameState: GameState;
   ownerId: string;
   status: string;
   createdAt: string;
   gameType: string;
   entityId: string;
+};
+
+export type GameState = {
+  player: Player;
+  moves: number;
+  timer: number;
+  start: Position;
+  startRotation: number;
+  target: Position;
+  rows: number;
+  columns: number;
+  square: number;
+  maze: number[][];
+};
+
+export type Player = {
+  position: Position;
+  rotation: number;
+};
+
+export type Position = {
+  x: number;
+  y: number;
 };
