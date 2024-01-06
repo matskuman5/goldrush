82733908ca6c0d694b51ea5ea703b0d5b416153e
Matias <selinmatias5@gmail.com>
2024-01-06 14:20:57 +0200

    remove unused types

--- src/types.ts
+++ src/types.ts
@@ -1,30 +1,3 @@
-export type GameInstance = {
-  gameState: GameState;
-  ownerId: string;
-  status: string;
-  createdAt: string;
-  gameType: string;
-  entityId: string;
-};
-
-export type GameState = {
-  player: Player;
-  moves: number;
-  timer: number;
-  start: Position;
-  startRotation: number;
-  target: Position;
-  rows: number;
-  columns: number;
-  square: number;
-  maze: number[][];
-};
-
-export type Player = {
-  position: Position;
-  rotation: number;
-};
-
 export type Position = {
   x: number;
   y: number;
